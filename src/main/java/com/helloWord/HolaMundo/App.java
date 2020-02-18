e428844b936307dbc922fdca895ab42298619d0f
JuanZepeda <juan.zepeda@agilethought.com>
2020-02-18 12:53:05 -0600

    newDocument

---
+++ src/main/java/com/helloWord/HolaMundo/App.java
@@ -0,0 +1,13 @@
+package com.helloWord.HolaMundo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
