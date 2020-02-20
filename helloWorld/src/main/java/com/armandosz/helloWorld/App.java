f7695df8faac168e743fd2733601ee9594fe71f2
Jose Sanchez Barajas <jose.sanchez@agilethought.com>
2020-02-20 08:21:44 -0600

    maven project

---
+++ helloWorld/src/main/java/com/armandosz/helloWorld/App.java
@@ -0,0 +1,13 @@
+package com.armandosz.helloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World this is my first maven project!" );
+    }
+}
