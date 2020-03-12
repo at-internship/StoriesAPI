756aeb5009231d8895edea5ce4a1e559de9abc7a
Jose Sanchez Barajas <jose.sanchez@agilethought.com>
2020-03-12 17:29:55 -0600

    Fixed code commit repository annotation

--- src/main/java/com/stories/repository/StoriesRepository.java
+++ src/main/java/com/stories/repository/StoriesRepository.java
@@ -1,9 +1,11 @@
 package com.stories.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.stories.model.StoryModel;
 
+@Repository
 public interface StoriesRepository extends MongoRepository<StoryModel, String> {
 
 }
