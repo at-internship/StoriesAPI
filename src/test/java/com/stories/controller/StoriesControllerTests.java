8684c5cf76e094a8aa56ad59b5357cc26bd96547
Jose Sanchez Barajas <jose.sanchez@agilethought.com>
2020-03-13 14:07:21 -0600

    Change Java Version

---
+++ src/test/java/com/stories/controller/StoriesControllerTests.java
@@ -0,0 +1,13 @@
+package com.stories.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@WebMvcTest(controllers = StoriesController.class)
+public class StoriesControllerTests {
+
+}
