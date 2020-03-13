377f0483c19ba1b0e7961d754b811a0861dc149e
apokochito <diana.apolinar@hotmail.com>
2020-03-12 19:16:01 -0600

    Change merge owner

--- src/main/java/com/stories/service/StoryService.java
+++ src/main/java/com/stories/service/StoryService.java
@@ -2,10 +2,17 @@ package com.stories.service;
 
 import com.stories.domain.StoryDomain;
 
+import java.util.List;
+
 public interface StoryService {
+
+	StoryDomain getStoryById(String id) throws Exception;
+
+	List<StoryDomain> getAllStories() throws Exception;
+
 	void createStory(StoryDomain request) throws Exception;
 
 	void deleteStory(String id) throws Exception;
 	
-	public StoryDomain updateStory(StoryDomain request, String id) throws Exception;
+	StoryDomain updateStory(StoryDomain request, String id) throws Exception;
 }
