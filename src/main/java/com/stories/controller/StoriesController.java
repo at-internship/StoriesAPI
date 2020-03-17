242f6b470c8c9082af112e3d758b3a24962d6920
Alejandro Gutierrez Aguilar <alejandro.gutierrez@agilethought.com>
2020-03-17 15:33:08 -0600

    Unit Test for PUT method and JaCoCo implementation

--- src/main/java/com/stories/controller/StoriesController.java
+++ src/main/java/com/stories/controller/StoriesController.java
@@ -1,14 +1,25 @@
 package com.stories.controller;
 
-import com.stories.domain.StoryDomain;
-import com.stories.service.StoryServiceImpl;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 import javax.validation.Valid;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stories.domain.StoryDomain;
+import com.stories.service.StoryServiceImpl;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -17,16 +28,16 @@ public class StoriesController {
 
 	@Autowired
 	StoryServiceImpl storyService;
-  
-  @ResponseStatus(value = HttpStatus.OK)
-  @GetMapping(value = "/", produces = "application/json")
-	public List<StoryDomain> getAllStories() throws Exception{
+
+	@ResponseStatus(value = HttpStatus.OK)
+	@GetMapping(value = "/", produces = "application/json")
+	public List<StoryDomain> getAllStories() throws Exception {
 		return storyService.getAllStories();
-	}	
-	
+	}
+
 	@ResponseStatus(value = HttpStatus.OK)
 	@GetMapping(value = "/{id}", produces = "application/json")
-	public StoryDomain getStoryById(@Valid @PathVariable String id) throws Exception{
+	public StoryDomain getStoryById(@Valid @PathVariable String id) throws Exception {
 		return storyService.getStoryById(id);
 	}
 
--- src/test/java/com/stories/controller/StoriesControllerTests.java
+++ src/test/java/com/stories/controller/StoriesControllerTests.java
@@ -1,13 +1,75 @@
 package com.stories.controller;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import junit.framework.TestCase;
+
 @WebAppConfiguration
-@WebMvcTest(controllers = StoriesController.class)
-public class StoriesControllerTests {
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class StoriesControllerTests extends TestCase {
 
+	protected MockMvc mvc;
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@Override
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void putTestTrue() throws Exception {
+		String uri = "/stories/5e713308b7872622cb3d10ea";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri).contentType(MediaType.APPLICATION_JSON_VALUE)
+				.contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
+				.content(getStoryInJson("5e713308b7872622cb3d10ea"))).andReturn();
+		
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(status, 202);
+	}
+
+	@Test
+	public void putTestInvelidId() throws Exception {
+		String uri = "/stories/5e6a8441bfc6533811235e1";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri).contentType(MediaType.APPLICATION_JSON_VALUE)
+				.contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
+				.content(getStoryInJson("5e6a8441bfc6533811235e1"))).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(status, 404);
+	}
+	
+	@Test
+	public void putTestInvalidJson() throws Exception {
+		String uri = "/stories/5e6a8441bfc6533811235e19";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri).contentType(MediaType.APPLICATION_JSON_VALUE)
+				.contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8")
+				.content(getStoryInJsonBad("5e6a8441bfc6533811235e19"))).andReturn();
+				
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(status, 400);
+	}
+
+	private String getStoryInJson(String id) {
+		return "{\"id\":\"" + id + "\", \"sprint_id\":\"UUID\", \"technology\":\"Java\",\"name\":\"Create Stories POST endpoint\", \"description\":\"\",\"acceptance_criteria\":\"\",\"points\":2,\"progress\":885, \"status\":\"Working\",\"notes\":\"\",\"comments\":\"Test\", \"start_date\":\"2020-08-25\",\"due_date\":\"2020-08-25\",\"priority\":\"High\", \"assignee_id\":\"UUID\",\"history\":[\"\",\"\"]}";
+	}
+	
+	private String getStoryInJsonBad(String id) {
+		return "{\"id\":\"" + id + "\", \"sprint_id\":\"UUID\", \"technology\":\"Java\",\"name\":\"Create Stories POST endpoint\", \"description\":\"\",\"acceptance_criteria\":\"\",\"points\":\"2#\",\"progress\":885, \"status\":\"Working\",\"notes\":\"\",\"comments\":\"Test\", \"start_date\":\"2020-08-25\",\"due_date\":\"2020-08-25\",\"priority\":\"High\", \"assignee_id\":\"UUID\",\"history\":[\"\",\"\"]}";
+	}
 }
