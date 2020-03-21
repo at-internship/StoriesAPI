a2bc97be6ef5b73f5975a773acd5e1c2ff9a1719
Alejandro Gutierrez Aguilar <alejandro.gutierrez@agilethought.com>
2020-03-20 20:44:47 -0600

    Maven plugin and Swagger UI  implementation

--- src/main/java/com/stories/controller/StoriesController.java
+++ src/main/java/com/stories/controller/StoriesController.java
@@ -1,52 +1,86 @@
 package com.stories.controller;
 
-import com.stories.domain.StoryDomain;
-import com.stories.service.StoriesServiceImpl;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 import javax.validation.Valid;
-import java.util.List;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stories.domain.StoryDomain;
+import com.stories.service.StoriesServiceImpl;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@Api(value = "Microservices STORY", tags = "Microservices STORY")
 @RequestMapping(value = "/stories")
 public class StoriesController {
 
-    @Autowired
-    StoriesServiceImpl storyService;
+	@Autowired
+	StoriesServiceImpl storyService;
+	
+	@ApiOperation(value = " GET Stories ", notes = " THIS OPERATION WILL RETURN A LIST OF STORIES ")
+	@ApiResponses({ @ApiResponse(code = 200, message = " SUCCESS OPERATION ") })
+	@ResponseStatus(value = HttpStatus.OK)
+	@GetMapping(value = "/", produces = "application/json")
+	public List<StoryDomain> getAllStories() throws Exception {
+		return storyService.getAllStories();
+	}
 
-    @ResponseStatus(value = HttpStatus.OK)
-    @GetMapping(value = "/", produces = "application/json")
-    public List<StoryDomain> getAllStories() throws Exception {
-        return storyService.getAllStories();
-    }
+	@ApiOperation(value = " GET Story ", notes = " THIS OPERATION WILL RETURN A STORY ")
+	@ApiResponses({ @ApiResponse(code = 200, message = " SUCCESS OPERATION "),
+			@ApiResponse(code = 404, message = " Story not found ") })
+	@ResponseStatus(value = HttpStatus.OK)
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public StoryDomain getStoryById(@Valid @PathVariable String id) throws Exception {
+		return storyService.getStoryById(id);
+	}
 
-    @ResponseStatus(value = HttpStatus.OK)
-    @GetMapping(value = "/{id}", produces = "application/json")
-    public StoryDomain getStoryById(@Valid @PathVariable String id) throws Exception {
-        return storyService.getStoryById(id);
-    }
+	@ApiOperation(value = " POST Story ", notes = " THIS OPERATION WILL ADD A STORY ")
+	@ApiResponses({ @ApiResponse(code = 201, message = " SUCCESS OPERATION "),
+			@ApiResponse(code = 400, message = " Story has an invalid status Json ") })
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@PostMapping(value = "/", consumes = "application/json", produces = "application/json")
+	public String createStory(@Valid @RequestBody StoryDomain request) throws Exception {
+		return storyService.createStory(request);
+	}
 
-    @ResponseStatus(value = HttpStatus.CREATED)
-    @PostMapping(value = "/", consumes = "application/json", produces = "application/json")
-    public String createStory(@Valid @RequestBody StoryDomain request) throws Exception {
-    	return storyService.createStory(request);
-    }
-
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping(value = "/{id}")
-    public void deleteStory(@Valid @PathVariable String id) throws Exception {
-        log.info("Deleting story with id from the controller: " + id);
-        storyService.deleteStory(id);
-    }
-
-    @ResponseStatus(value = HttpStatus.ACCEPTED)
-    @PutMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
-    public StoryDomain updateStory(@Valid @RequestBody StoryDomain request, @PathVariable String id) throws Exception {
-        return storyService.updateStory(request, id);
-    }
+	@ApiOperation(value = " DELETE Story ", notes = " THIS OPERATION WILL DELETE A STORY ")
+	@ApiResponses({ @ApiResponse(code = 204, message = " SUCCESS OPERATION "),
+			@ApiResponse(code = 404, message = " Status json state is invalid\", \"The status should be: Ready to Work, Working, Testing, Ready to Accept or Accepted ") })
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping(value = "/{id}")
+	public void deleteStory(@Valid @PathVariable String id) throws Exception {
+		log.info("Deleting story with id from the controller: " + id);
+		storyService.deleteStory(id);
+	}
+	
+	@ApiOperation(value = " PUT Story ", notes = " THIS OPERATION WILL UPDATE A STORY ")
+	@ApiResponses({ @ApiResponse(code = 202, message = " SUCCESS OPERATION "),
+			@ApiResponse(code = 404, message = " Story not found "),
+			@ApiResponse(code = 400, message = " Malformed JSON request ") })
+	@ResponseStatus(value = HttpStatus.ACCEPTED)
+	@PutMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
+	public StoryDomain updateStory(@Valid @RequestBody StoryDomain request, @PathVariable String id) throws Exception {
+		return storyService.updateStory(request, id);
+	}
 }
--- src/main/java/com/stories/domain/StoryDomain.java
+++ src/main/java/com/stories/domain/StoryDomain.java
@@ -7,32 +7,63 @@ import javax.validation.constraints.NotBlank;
 
 import org.springframework.data.mongodb.core.index.Indexed;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class StoryDomain {
-
+	
+	@ApiModelProperty(example="1", value="Identifier of the sprint")
 	private String sprint_id;
+	
+	@ApiModelProperty(example="Java", value="Technology used")
 	private String technology;
+	
+	@ApiModelProperty(example="Create new story", value="Name of the story")
 	@Indexed(unique = true)
 	@NotBlank(message = "name is required")
 	private String name;
+	
+	@ApiModelProperty(example="Make new Stories", value="Story description")
 	private String description;
+	
+	@ApiModelProperty(example="1", value="acceptance_criteria of the story")
 	private String acceptance_criteria;
+	
+	@ApiModelProperty(example="1", value="points of the story")
 	@Min(1)
 	private int points;
+	
+	@ApiModelProperty(example="1", value="progress of the story")
 	@Min(1)
 	private int progress;
+	
+	@ApiModelProperty(example="Working", value="status of the story")
 	@NotBlank(message = "Status is required")
 	private String status;
+	
+	@ApiModelProperty(example="The first steps", value="notes of the story")
 	private String notes;
+	
+	@ApiModelProperty(example="That do you don't", value="comments of the story")
 	private String comments;
+	
+	@ApiModelProperty(example="2020-08-25", value="start_date of the story")
 	private LocalDate start_date;
+	
+	@ApiModelProperty(example="2020-08-25", value="due_date of the story")
 	private LocalDate due_date;
+	
+	@ApiModelProperty(example="High", value="priority of the story")
 	private String priority;
+	
+	@ApiModelProperty(example="1", value="assignee_id of the story")
 	private String assignee_id;
+	
+	@ApiModelProperty(example="1", value="the history of the story")
 	private List<String> history;
 
 	public StoryDomain() {
