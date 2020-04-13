d73249d60f5c952fc8561005e28477f52827c7cb
Jose Sanchez Barajas <jose.sanchez@agilethought.com>
2020-04-13 11:16:20 -0500

    fixed blank spaces

--- src/main/java/com/stories/service/StoriesServiceImpl.java
+++ src/main/java/com/stories/service/StoriesServiceImpl.java
@@ -50,6 +50,7 @@ public class StoriesServiceImpl implements StoriesService {
 	List<StoryModel> storiesModel = new ArrayList<StoryModel>();
 	StoryDomain storyDomain = new StoryDomain();
 	List<StoryDomain> storiesDomain = new ArrayList<>();
+	
 
 	@Autowired
 	SprintsClient sprintClient;
@@ -65,48 +66,49 @@ public class StoriesServiceImpl implements StoriesService {
 			String id = nameValidation(storyModel).get_id();
 			return id;
 		} else {
-			if (mensaggeDinamicValidation[2] == "BAD_REQUEST") {
-				throw new EntityNotFoundException(mensaggeDinamicValidation[0], "", mensaggeDinamicValidation[1]);
-			} else if (mensaggeDinamicValidation[2] == "CONFLICT") {
-				throw new EntityNotFoundException(mensaggeDinamicValidation[0], HttpStatus.CONFLICT,
-						mensaggeDinamicValidation[1]);
-			} else {
-				throw new EntityNotFoundException(mensaggeDinamicValidation[0], mensaggeDinamicValidation[1]);
-			}
+			if(mensaggeDinamicValidation[2] == "BAD_REQUEST") {
+                throw new EntityNotFoundException(mensaggeDinamicValidation[0],"", mensaggeDinamicValidation[1]);
+            }
+            else if(mensaggeDinamicValidation[2] == "CONFLICT") {
+                throw new EntityNotFoundException(mensaggeDinamicValidation[0], HttpStatus.CONFLICT, mensaggeDinamicValidation[1]);
+            }
+            else {
+                throw new EntityNotFoundException(mensaggeDinamicValidation[0], mensaggeDinamicValidation[1]);
+            }
 		}
 	}
-
+	
 	@Override
 	public String createTask(TasksDomain taskDomain, String id) throws Exception {
 		TaskModel taskModel = new TaskModel();
-		if (storiesRepository.existsById(id)) {
-			if (userNullTaskValidation(taskDomain.getAssignee())) {
-				if (statusTaskValidation(statusArray, taskDomain.getStatus())) {
-					storyModel = storiesRepository.findById(id).get();
-					List<TaskModel> tasks = storyModel.getTasks();
-					taskModel = mapperFacade.map(taskDomain, TaskModel.class);
-					taskModel.set_id(new ObjectId().toString());
-					tasks.add(taskModel);
-					storyModel.setTasks(tasks);
-					storiesRepository.save(storyModel);
-					return taskModel.get_id();
-				} else {
-					throw new EntityNotFoundException(
-							"The Status field should be one of the following options: 'Refining' ,'Ready to Work', 'Working', 'Testing', 'Ready to Accept' or 'Accepted'.",
-							"400", "/stories/");
-				}
-			} else {
-				throw new EntityNotFoundException("The user provided does not exist", "/users/");
-			}
-		} else {
-			throw new EntityNotFoundException("Story not found", HttpStatus.CONFLICT, "/stories/");
+		   if(storiesRepository.existsById(id)) {
+		      if (userNullTaskValidation(taskDomain.getAssignee())) {
+		         if (statusTaskValidation(statusArray, taskDomain.getStatus())) {
+		            storyModel = storiesRepository.findById(id).get();
+		            List<TaskModel> tasks = storyModel.getTasks();
+		            taskModel = mapperFacade.map(taskDomain, TaskModel.class);
+		            taskModel.set_id(new ObjectId().toString());
+		            tasks.add(taskModel);
+		            storyModel.setTasks(tasks);
+		            storiesRepository.save(storyModel);
+		            return taskModel.get_id();
+		         }else {
+		            throw new EntityNotFoundException(
+		                  "The Status field should be one of the following options: 'Refining' ,'Ready to Work', 'Working', 'Testing', 'Ready to Accept' or 'Accepted'.",
+		                  "400","/stories/");
+		         }
+		      }else {
+		         throw new EntityNotFoundException("The user provided does not exist", "/users/");
+		      }
+		   }else {
+		         throw new EntityNotFoundException("Story not found", HttpStatus.CONFLICT,"/stories/");
+		      }
 		}
-	}
 
 	@Override
 	public void deleteStory(String id) throws Exception {
 		if (!storiesRepository.existsById(id)) {
-			throw new EntityNotFoundException("Story with the given id was not found", "/stories/");
+			throw new EntityNotFoundException("Story with the given id was not found","/stories/");
 		} else
 			logger.debug("Deleting story with the id: " + id);
 		storiesRepository.deleteById(id);
@@ -115,7 +117,7 @@ public class StoriesServiceImpl implements StoriesService {
 	@Override
 	public void deleteTask(String id, String taskId) throws Exception {
 		if (!storiesRepository.existsById(id)) {
-			throw new EntityNotFoundException("Story with the given id was not found", "/stories/");
+			throw new EntityNotFoundException("Story with the given id was not found","/stories/");
 		} else {
 			storyModel = storiesRepository.findById(id).get();
 			List<TaskModel> tasks = storyModel.getTasks();
@@ -154,57 +156,56 @@ public class StoriesServiceImpl implements StoriesService {
 				throw new EntityNotFoundException("Story not found", "/stories/");
 			}
 		} else {
-			if (mensaggeDinamicValidation[2] == "BAD_REQUEST") {
-				throw new EntityNotFoundException(mensaggeDinamicValidation[0], "", mensaggeDinamicValidation[1]);
-			} else if (mensaggeDinamicValidation[2] == "CONFLICT") {
-				throw new EntityNotFoundException(mensaggeDinamicValidation[0], HttpStatus.CONFLICT,
-						mensaggeDinamicValidation[1]);
-			} else {
-				throw new EntityNotFoundException(mensaggeDinamicValidation[0], mensaggeDinamicValidation[1]);
-			}
+			if(mensaggeDinamicValidation[2] == "BAD_REQUEST") {
+                throw new EntityNotFoundException(mensaggeDinamicValidation[0],"", mensaggeDinamicValidation[1]);
+            }
+            else if(mensaggeDinamicValidation[2] == "CONFLICT") {
+                throw new EntityNotFoundException(mensaggeDinamicValidation[0], HttpStatus.CONFLICT, mensaggeDinamicValidation[1]);
+            }
+            else {
+                throw new EntityNotFoundException(mensaggeDinamicValidation[0], mensaggeDinamicValidation[1]);
+            }
 		}
 	}
-
+	
 	@Override
 	public TasksDomain updateTaskById(TasksDomain task, String id, String _id) throws Exception {
-		if (storiesRepository.existsById(id)) {
+		if(storiesRepository.existsById(id)) {
 			task.set_id(_id);
 			TaskModel taskModel = storiesCustomRepository.getTaskById(id, _id).getUniqueMappedResult();
-			if (taskModel == null) {
-				throw new EntityNotFoundException("Task not found", "/stories/" + id + "/tasks/" + _id);
-			}
+			if(taskModel == null) {
+    			throw new EntityNotFoundException("Task not found", "/stories/" + id + "/tasks/" + _id);
+    		}
 			if (!statusTaskValidation(statusArray, task.getStatus())) {
 				throw new EntityNotFoundException(
-						"The Status field should be one of the following options: 'Refining' ,'Ready to Work', 'Working', 'Testing', 'Ready to Accept' or 'Accepted'.",
-						"400", "/stories/");
+		                  "The Status field should be one of the following options: 'Refining' ,'Ready to Work', 'Working', 'Testing', 'Ready to Accept' or 'Accepted'.",
+		                  "400","/stories/");
 			}
-			if (!StringUtils.isEmpty(task.getAssignee())) {
-				if (!usersRepository.existsById(task.getAssignee())) {
-					throw new EntityNotFoundException("User assignee id does not exist", HttpStatus.CONFLICT,
-							"/stories/" + id + "/tasks/" + _id);
+			if(!StringUtils.isEmpty(task.getAssignee())) {
+				if(!usersRepository.existsById(task.getAssignee())) {
+					throw new EntityNotFoundException("User assignee id does not exist", HttpStatus.CONFLICT, "/stories/" + id + "/tasks/" + _id);
 				}
 			}
-			if (StringUtils.isEmpty(task.getName())) {
-				throw new EntityNotFoundException("name is a required field", HttpStatus.BAD_REQUEST,
-						"/stories/" + id + "/tasks/" + _id);
+			if(StringUtils.isEmpty(task.getName())) {
+				throw new EntityNotFoundException("name is a required field", HttpStatus.BAD_REQUEST, "/stories/" + id + "/tasks/" + _id);
 			}
 			storyModel = storiesRepository.findById(id).get();
 			List<TaskModel> updatedTasks = new ArrayList<>();
-			for (TaskModel tasks : storyModel.getTasks()) {
-				if (task.get_id().equals(tasks.get_id())) {
-					tasks.set_id(task.get_id());
-					tasks.setName(task.getName());
-					tasks.setDescription(task.getDescription());
-					tasks.setStatus(task.getStatus());
-					tasks.setComments(task.getComments());
-					tasks.setAssignee(task.getAssignee());
-				}
-				updatedTasks.add(tasks);
-			}
+			for(TaskModel tasks: storyModel.getTasks()) {
+                if(task.get_id().equals(tasks.get_id())) {
+                    tasks.set_id(task.get_id());
+                    tasks.setName(task.getName());
+                    tasks.setDescription(task.getDescription());
+                    tasks.setStatus(task.getStatus());
+                    tasks.setComments(task.getComments());
+                    tasks.setAssignee(task.getAssignee());    
+                }
+                updatedTasks.add(tasks);   
+            }
 			storyModel.setTasks(updatedTasks);
 			storiesRepository.save(storyModel);
-			return task;
-		} else {
+    		return task;
+		}else {
 			throw new EntityNotFoundException("Story not found", "/stories/" + id);
 		}
 	}
@@ -252,7 +253,6 @@ public class StoriesServiceImpl implements StoriesService {
 			return results;
 		}
 		throw new EntityNotFoundException("Story not found", "/stories/" + id + "/tasks");
-
 	}
 
 	private String statusValidation(String[] statusArray, String storyStatus) {
@@ -266,7 +266,7 @@ public class StoriesServiceImpl implements StoriesService {
 			return validationStatus + ", ";
 		}
 	}
-
+	
 	private StoryModel nameValidation(StoryModel storyModel) throws EntityNotFoundException {
 		try {
 			storiesRepository.save(storyModel);
@@ -334,11 +334,9 @@ public class StoriesServiceImpl implements StoriesService {
 		if (progress < 0) {
 			progressValidation = "The number entered in the progress field is a negative number";
 		}
-
 		if (progress > 100) {
 			progressValidation = "The number entered in the progress field exceeds 100%";
 		}
-
 		if (StringUtils.isEmpty(progressValidation)) {
 			return progressValidation;
 		} else {
@@ -367,105 +365,103 @@ public class StoriesServiceImpl implements StoriesService {
 	}
 
 	private String[] dynamicValidation(StoryDomain storyDomain) {
-		String[] mensaggeDinamicValidation = { "", "", "" };
-		String validationRespons = "";
-		String[] validationPath = { "/Sprints/", "/StoryDomain/", "/Users/", "/stories/" };
+		String[] mensaggeDinamicValidation = { "", "" , ""};
+        String validationRespons = "";
+        String[] validationPath = { "/Sprints/", "/StoryDomain/", "/Users/", "/stories/" };
 
-		validationRespons = nameStatusNullValidation(storyDomain.getName(), storyDomain.getStatus());
-		if (!StringUtils.isEmpty(validationRespons)) {
-			mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
-			mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
-			mensaggeDinamicValidation[2] = "BAD_REQUEST";
-			return mensaggeDinamicValidation;
-		}
+        validationRespons = nameStatusNullValidation(storyDomain.getName(), storyDomain.getStatus());
+        if (!StringUtils.isEmpty(validationRespons)) {
+            mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
+            mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
+            mensaggeDinamicValidation[2] = "BAD_REQUEST";
+            return mensaggeDinamicValidation;
+        }
 
-		if ((!StringUtils.isEmpty(storyDomain.getSprint_id()))
-				|| (!StringUtils.isEmpty(storyDomain.getAssignee_id()))) {
-			validationRespons = sprintNullValidation(storyDomain.getSprint_id());
-			if (!StringUtils.isEmpty(validationRespons)) {
-				mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
-				mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
+        if((!StringUtils.isEmpty(storyDomain.getSprint_id())) || (!StringUtils.isEmpty(storyDomain.getAssignee_id()))) {
+            validationRespons = sprintNullValidation(storyDomain.getSprint_id());
+            if (!StringUtils.isEmpty(validationRespons)) {
+                mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
+                mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
+                
+            }
+            
+            validationRespons = userNullValidation(storyDomain.getAssignee_id());
+            if (!StringUtils.isEmpty(validationRespons)) {
+                mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
+                mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
+            }
+            
+            mensaggeDinamicValidation[1] = filtervalidation(validationPath, mensaggeDinamicValidation[1]);
+            mensaggeDinamicValidation[2] = "CONFLICT";
+            if(!StringUtils.isEmpty(mensaggeDinamicValidation[0])) {
+                return mensaggeDinamicValidation;    
+            }
+        }
 
-			}
+        if((!StringUtils.isEmpty(storyDomain.getStatus())) || !(StringUtils.isEmpty(storyDomain.getProgress()+"")) || !(StringUtils.isEmpty(storyDomain.getPoints()+""))) {
+            validationRespons = pointsValidation(storyDomain.getPoints(), pointsArray);
+            if (!StringUtils.isEmpty(validationRespons)) {
+                mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
+                mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
+            }
 
-			validationRespons = userNullValidation(storyDomain.getAssignee_id());
-			if (!StringUtils.isEmpty(validationRespons)) {
-				mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
-				mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
-			}
+            validationRespons = proggressValidation(storyDomain.getProgress());
+            if (!StringUtils.isEmpty(validationRespons)) {
+                mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
+                mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
+            }
 
-			mensaggeDinamicValidation[1] = filtervalidation(validationPath, mensaggeDinamicValidation[1]);
-			mensaggeDinamicValidation[2] = "CONFLICT";
-			if (!StringUtils.isEmpty(mensaggeDinamicValidation[0])) {
-				return mensaggeDinamicValidation;
-			}
-		}
-
-		if ((!StringUtils.isEmpty(storyDomain.getStatus())) || !(StringUtils.isEmpty(storyDomain.getProgress() + ""))
-				|| !(StringUtils.isEmpty(storyDomain.getPoints() + ""))) {
-			validationRespons = pointsValidation(storyDomain.getPoints(), pointsArray);
-			if (!StringUtils.isEmpty(validationRespons)) {
-				mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
-				mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
-			}
-
-			validationRespons = proggressValidation(storyDomain.getProgress());
-			if (!StringUtils.isEmpty(validationRespons)) {
-				mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
-				mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
-			}
-
-			validationRespons = statusValidation(statusArray, storyDomain.getStatus());
-			if (!StringUtils.isEmpty(validationRespons)) {
-				mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
-				mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
-			}
-
-			mensaggeDinamicValidation[1] = filtervalidation(validationPath, mensaggeDinamicValidation[1]);
-			mensaggeDinamicValidation[2] = "BAD_REQUEST";
-			if (!StringUtils.isEmpty(mensaggeDinamicValidation[0])) {
-				return mensaggeDinamicValidation;
-			}
-		}
-
-		mensaggeDinamicValidation[1] = validationRespons;
-		return mensaggeDinamicValidation;
+            validationRespons = statusValidation(statusArray, storyDomain.getStatus());
+            if (!StringUtils.isEmpty(validationRespons)) {
+                mensaggeDinamicValidation[0] = mensaggeDinamicValidation[0] + validationRespons;
+                mensaggeDinamicValidation[1] = mensaggeDinamicValidation[1] + "/stories/";
+            }
+            
+            mensaggeDinamicValidation[1] = filtervalidation(validationPath, mensaggeDinamicValidation[1]);
+            mensaggeDinamicValidation[2] = "BAD_REQUEST";
+            if(!StringUtils.isEmpty(mensaggeDinamicValidation[0])) {
+                return mensaggeDinamicValidation;
+            }
+        }
+        mensaggeDinamicValidation[1] = validationRespons;
+        return mensaggeDinamicValidation;
 	}
-
+	
 	private String filtervalidation(String[] validationPath, String string) {
-		String validationRespons = "";
-		for (int i = 0; i < validationPath.length; i++) {
-			if (string.toString().indexOf(validationPath[i]) == -1) {
+        String validationRespons = "";
+        for (int i = 0; i < validationPath.length; i++) {
+            if (string.toString().indexOf(validationPath[i]) == -1) {
 
-			} else {
-				if (validationPath[i].equals(string.toString().substring(string.toString().indexOf(validationPath[i]),
-						string.toString().indexOf(validationPath[i]) + validationPath[i].length()))) {
-					if (!(string.toString().indexOf(validationPath[i]) == -1)) {
-						if (StringUtils.isEmpty(validationRespons)) {
-							validationRespons = validationPath[i];
-						}
-					}
-				}
-			}
-		}
-		return validationRespons;
-	}
-
+            } else {
+                if (validationPath[i].equals(string.toString().substring(
+                        string.toString().indexOf(validationPath[i]),
+                        string.toString().indexOf(validationPath[i])
+                                + validationPath[i].length()))) {
+                    if (!(string.toString().indexOf(validationPath[i]) == -1)) {
+                        if (StringUtils.isEmpty(validationRespons)) {
+                            validationRespons = validationPath[i];
+                        }
+                    }
+                }
+            }
+        }
+        return validationRespons;
+    }
+	
 	private boolean userNullTaskValidation(String assigneeId) throws EntityNotFoundException {
 		if (StringUtils.isEmpty(assigneeId)) {
 			return true;
 		} else {
 			if (!usersRepository.existsById(assigneeId))
-				throw new EntityNotFoundException("The user provided does not exist", HttpStatus.CONFLICT, "/stories/");
+				throw new EntityNotFoundException("The user provided does not exist",HttpStatus.CONFLICT,"/stories/");
 			return true;
 		}
 	}
-
 	private boolean statusTaskValidation(String[] statusArray, String status) {
-		if (StringUtils.isEmpty(status)) {
+		if(StringUtils.isEmpty(status)) {
 			return true;
-		} else {
-			return Arrays.asList(statusArray).contains(status);
+		}else {
+		return Arrays.asList(statusArray).contains(status);
 		}
 	}
 }
