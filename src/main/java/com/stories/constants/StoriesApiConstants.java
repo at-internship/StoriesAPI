12527a99b11695c6ad23f659b6f21c4f816d18bb
Alejandro Gutierrez Aguilar <alejandro.gutierrez@agilethought.com>
2020-04-27 20:18:28 -0500

    karina changes and validation with regex

--- src/main/java/com/stories/constants/StoriesApiConstants.java
+++ src/main/java/com/stories/constants/StoriesApiConstants.java
@@ -2,7 +2,7 @@ package com.stories.constants;
 
 public class StoriesApiConstants {
 		public static final String[] statusArray = { "Refining", "Ready to Work", "Working", "Testing", "Ready to Accept", "Accepted" };
-		public static final String[] storyDomainValidationArray = { "Story_id", "Sprint_id", "Status", "Priority", "Assignee_id" };
+		public static final String[] storyDomainValidationArray = { "Story_id", "Sprint_id", "Status", "Assignee_id" };
 		public static final String[] taskDomainValidationArray = { "Story_id", "Task_id", "Status", "assignee" };
 		public static final int[] pointsArray = { 0, 1, 2, 3, 5 };
 		public static final String[] specialCharactersArray = {"?","!","(",")","$","&","/","@","^",">","<","|","#",";",":","[","]","{","}","+","*","¨","¿","-","=","%",",","'",".","_","°","~"};
@@ -26,6 +26,9 @@ public class StoriesApiConstants {
 		public static final String storyFieldIdNotFoundException = "Story with the given id was not found.";
 		public static final String storyFieldSprintIdDoesntExistException = "The id entered in the sprint_id field does not exist";
 		public static final String storyFieldAssigneDoesntExistException = "User assignee_id does not exist";
+		public static final String storyFieldStoryIdSpecialCharacter = "Story not found";
+		public static final String storyFieldSprintIdSpecialCharacter = "The id entered in the sprint_id field does not exist";
+		public static final String storyFieldAssigneSpecialCharacter = "User assignee_id does not exist";
 		public static final String storyFieldProgressNegativeException = "The number entered in the progress field is a negative number";
 		public static final String storyFieldProgressExceedsException = "The number entered in the progress field exceeds 100%";
 		public static final String storyFieldPointsNegativeException = "The number entered in the points field is a negative number";
@@ -34,9 +37,12 @@ public class StoriesApiConstants {
 		public static final String taskFieldAssigneeNotFoundException = "User assignee does not exist";
 		public static final String taskFieldIdNotFoundException = "Task with the given id was not found.";
 		public static final String taskFieldNameRequiredException = "The JSON format provided is invalid, please provide the required field ('Name').";
+		public static final String storyFieldTaskIdSpecialCharacter = "Task not found";
 		
 		public static final String exceptionCharactersInFollowingFields = "We're not handling special characters, please provide a proper value in the following fields: ";
 		public static final String exceptionCharactersInSpecificField = "We're not handling special characters, please provide a proper value in the next field: ";
 		
+		public static final String[] ValidationStorySpecialCharacterMessage = {storyFieldStoryIdSpecialCharacter, storyFieldSprintIdSpecialCharacter, storyFieldStatusInvalidException, storyFieldAssigneSpecialCharacter};
+		public static final String[] ValidationTaskSpecialCharacterMessage = {storyFieldStoryIdSpecialCharacter, storyFieldTaskIdSpecialCharacter, storyFieldStatusInvalidException, storyFieldAssigneSpecialCharacter};
 }		
 		
