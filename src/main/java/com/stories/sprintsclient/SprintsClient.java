eceb40753de9218de2d6a6247ba0bffb381cddd5
Jose Sanchez Barajas <jose.sanchez@agilethought.com>
2020-04-29 11:42:48 -0500

    Add Loggin for SprintClient

--- src/main/java/com/stories/sprintsclient/SprintsClient.java
+++ src/main/java/com/stories/sprintsclient/SprintsClient.java
@@ -2,6 +2,8 @@ package com.stories.sprintsclient;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,9 @@ import com.stories.domain.SprintDomain;
 
 @Component
 public class SprintsClient {
-
+	
+	private static Logger logger = LogManager.getLogger();
+	
 	RestTemplate restTemplate = new RestTemplate();
 
 	public boolean existsSprintById(String id) throws RestClientException {
@@ -26,9 +30,11 @@ public class SprintsClient {
 
 			if (sprintsResponse != null && sprintsResponse.hasBody()) {
 				List<SprintDomain> sprints = sprintsResponse.getBody();
+				logger.debug("Getting all sprints: " + sprints);
 				for (int i = 0; i < sprints.size(); i++) {
 					if (id.equals(sprints.get(i).getId())) {
 						exists = true;
+						logger.debug("Getting sprint if exist: " + sprints.get(i));
 						break;
 					}
 				}
